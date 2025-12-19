ba05a6983fa1cf2ec5177e3be7570842edf8d02c
okamoto-ryoki <ryoki.okamoto@asia-quest.jp>
2025-12-19 12:38:18 +0900

    new-todo

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,3 @@
-// import { HashRouter as Router, Routes, Route  } from 'react-router-dom';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './index.css'
